a5e641d0c0c920f3f2488fe72edc998c98d1cb89
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-13 15:29:29 -0500

    list access and search

--- lists.kt
+++ lists.kt
@@ -2,4 +2,8 @@ fun main() {
   val solarSystem =
       listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
   println(solarSystem.size)
+  println(solarSystem[2])
+  println(solarSystem.get(3))
+  println(solarSystem.indexOf("Earth"))
+  println(solarSystem.indexOf("Pluto"))
 }
