c566a48c97ee2e44f97593a41a391ed111f1dcb5
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-13 15:58:55 -0500

    fold/reduce

--- collection-hofs.kt
+++ collection-hofs.kt
@@ -22,13 +22,16 @@ fun main() {
   // println("Soft cookies:")
   // softBakedMenu.forEach { println("${it.name} - $${it.price}") }
 
-  val groupedMenu = cookies.groupBy { it.softBaked }
-  val softBakedMenu = groupedMenu[true] ?: emptyList()
-  val crunchyMenu = groupedMenu[false] ?: emptyList()
+  // val groupedMenu = cookies.groupBy { it.softBaked }
+  // val softBakedMenu = groupedMenu[true] ?: emptyList()
+  // val crunchyMenu = groupedMenu[false] ?: emptyList()
 
-  println("Soft cookies:")
-  softBakedMenu.forEach { println("${it.name} - $${it.price}") }
-  println()
-  println("Crunchy cookies:")
-  crunchyMenu.forEach { println("${it.name} - $${it.price}") }
+  // println("Soft cookies:")
+  // softBakedMenu.forEach { println("${it.name} - $${it.price}") }
+  // println()
+  // println("Crunchy cookies:")
+  // crunchyMenu.forEach { println("${it.name} - $${it.price}") }
+
+  val totalPrice = cookies.fold(0.0) { total, cookie -> total + cookie.price }
+  println("Total price: $$totalPrice")
 }
