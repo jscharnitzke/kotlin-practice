c8f88ed5ffd2c4f9704bb17eee025709f3e81cbc
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-13 15:03:36 -0500

    add instructional comments

--- oop-intermediate.kt
+++ oop-intermediate.kt
@@ -14,15 +14,19 @@ class Quiz {
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
 
+    // companion class (`companion`): properties can be accessed directly
     companion object StudentProgress {
+        // singleton class (`object`): can't be instantiated only accessed directly
         var total: Int = 10
         var answered: Int = 3
 	}
 }
 
+// extend a class with a property; can customize getters and setters
 val Quiz.StudentProgress.progressText: String
 	get() = "$answered of $total answered."
 
+// extend a class with a method
 fun Quiz.StudentProgress.printProgressBar() {
     repeat(Quiz.answered) { print("▓") }
     repeat(Quiz.total - Quiz.answered) { print("▒") }
