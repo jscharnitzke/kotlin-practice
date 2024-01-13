e5c3031015752dd6e12b7698b01bb4ba25da0d74
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-13 15:00:46 -0500

    extend Quiz with a new function

--- oop-intermediate.kt
+++ oop-intermediate.kt
@@ -23,6 +23,13 @@ class Quiz {
 val Quiz.StudentProgress.progressText: String
 	get() = "$answered of $total answered."
 
-fun main() {
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
     println(Quiz.progressText)
+}
+
+fun main() {
+    println(Quiz.printProgressBar())
 }
