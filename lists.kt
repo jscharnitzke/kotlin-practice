66258af3fea2d85ef01a3674dbf26bd81cc711b1
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-13 15:34:37 -0500

    list deletion and search

--- lists.kt
+++ lists.kt
@@ -17,4 +17,9 @@ fun main() {
   solarSystem[3] = "Future Moon"
   println(solarSystem[3])
   println(solarSystem[9])
+
+  solarSystem.removeAt(9)
+  solarSystem.remove("Future Moon")
+  println(solarSystem.contains("Pluto"))
+  println("Future Moon" in solarSystem)
 }
