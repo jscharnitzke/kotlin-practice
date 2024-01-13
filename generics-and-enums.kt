79c4cad25ac1ee4b2d1f716e6e67c34cdeb88b6a
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-13 14:47:30 -0500

    generic example

---
+++ generics-and-enums.kt
@@ -0,0 +1,11 @@
+class Question<AnswerType>(
+    val questionText: String,
+    val answer: AnswerType,
+    val difficulty: String
+)
+
+fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
+    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
+    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+}
