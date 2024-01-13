fbb72f9d1714fe0950fa56a2b170a8afc037d85d
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-13 14:49:19 -0500

    refactor to use enum for difficulty

--- generics-and-enums.kt
+++ generics-and-enums.kt
@@ -1,11 +1,15 @@
 class Question<AnswerType>(
     val questionText: String,
     val answer: AnswerType,
-    val difficulty: String
+    val difficulty: Difficulty
 )
 
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+
 fun main() {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
-    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
-    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
 }
