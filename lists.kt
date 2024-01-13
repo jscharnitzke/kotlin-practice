3911e8173d130a91762615a917a9b3b383eefa0b
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-13 15:30:41 -0500

    for loops

--- lists.kt
+++ lists.kt
@@ -6,4 +6,8 @@ fun main() {
   println(solarSystem.get(3))
   println(solarSystem.indexOf("Earth"))
   println(solarSystem.indexOf("Pluto"))
+
+  for (planet in solarSystem) {
+    println(planet)
+  }
 }
