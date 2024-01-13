c6eda3bc092f7a0a1e642a5e00420df090c461f2
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-13 15:23:20 -0500

    demonstrate basic kotlin arrays

---
+++ collections.kt
@@ -0,0 +1,6 @@
+fun main() {
+  val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
+  val rockPlanets = arrayOf("Mercury", "Venus", "Earth", "Mars")
+
+  val solarSystem = rockPlanets + gasPlanets
+}
