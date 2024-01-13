b17ec11111d681aa96f724a1fb89026777d83aaa
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-13 14:50:46 -0500

    change Question to a data class

--- generics-and-enums.kt
+++ generics-and-enums.kt
@@ -1,4 +1,4 @@
-class Question<AnswerType>(
+data class Question<AnswerType>(
     val questionText: String,
     val answer: AnswerType,
     val difficulty: Difficulty
@@ -12,4 +12,5 @@ fun main() {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+    println(question1.toString())
 }
