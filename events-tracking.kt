0e74f97922bf3d4ed978fba578706aa0229bdf39
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-13 16:10:50 -0500

    summarize a user's short events

--- events-tracking.kt
+++ events-tracking.kt
@@ -11,27 +11,46 @@ data class Event(
     val durationInMinutes: Int
 )
 
+class User(val events: MutableList<Event>) {
+  fun summarizeShortEvents() {
+    val shortEvents = events.filter { it.durationInMinutes < 60 }
+    println("You have ${shortEvents.size} short events.")
+  }
+}
+
 fun main() {
-  val events =
-      mutableListOf(
-          Event(
-              title = "Wake up",
-              description = "Time to get up",
-              daypart = DayPart.MORNING,
-              durationInMinutes = 0
-          ),
-          Event(title = "Eat breakfast", daypart = DayPart.MORNING, durationInMinutes = 15),
-          Event(title = "Learn about Kotlin", daypart = DayPart.AFTERNOON, durationInMinutes = 30),
-          Event(title = "Practice Compose", daypart = DayPart.AFTERNOON, durationInMinutes = 60),
-          Event(
-              title = "Watch latest DevBytes video",
-              daypart = DayPart.AFTERNOON,
-              durationInMinutes = 10
-          ),
-          Event(
-              title = "Check out latest Android Jetpack library",
-              daypart = DayPart.EVENING,
-              durationInMinutes = 45
-          ),
+  val user =
+      User(
+          mutableListOf(
+              Event(
+                  title = "Wake up",
+                  description = "Time to get up",
+                  daypart = DayPart.MORNING,
+                  durationInMinutes = 0
+              ),
+              Event(title = "Eat breakfast", daypart = DayPart.MORNING, durationInMinutes = 15),
+              Event(
+                  title = "Learn about Kotlin",
+                  daypart = DayPart.AFTERNOON,
+                  durationInMinutes = 30
+              ),
+              Event(
+                  title = "Practice Compose",
+                  daypart = DayPart.AFTERNOON,
+                  durationInMinutes = 60
+              ),
+              Event(
+                  title = "Watch latest DevBytes video",
+                  daypart = DayPart.AFTERNOON,
+                  durationInMinutes = 10
+              ),
+              Event(
+                  title = "Check out latest Android Jetpack library",
+                  daypart = DayPart.EVENING,
+                  durationInMinutes = 45
+              ),
+          )
       )
+
+  user.summarizeShortEvents()
 }
