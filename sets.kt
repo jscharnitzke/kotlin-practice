17e231ce736b8516735afca9d82991af0cdfb406
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-13 15:38:41 -0500

    add sets practice

---
+++ sets.kt
@@ -0,0 +1,10 @@
+fun main() {
+  val solarSystem =
+      mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+
+  println(solarSystem.size)
+
+  solarSystem.add("Pluto")
+  println(solarSystem.size)
+  println(solarSystem.contains("Pluto"))
+}
