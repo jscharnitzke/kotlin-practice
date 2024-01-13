49cd2fdfa8c555d157a56e2aa7e1bbfb24fd798f
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-13 16:07:37 -0500

    add a mutable list of events

--- events-tracking.kt
+++ events-tracking.kt
@@ -12,12 +12,26 @@ data class Event(
 )
 
 fun main() {
-  println(
-      Event(
-          "Study Kotlin",
-          "Commit to studying Kotlin at least 15 minutes per day.",
-          DayPart.EVENING,
-          15
+  val events =
+      mutableListOf(
+          Event(
+              title = "Wake up",
+              description = "Time to get up",
+              daypart = DayPart.MORNING,
+              durationInMinutes = 0
+          ),
+          Event(title = "Eat breakfast", daypart = DayPart.MORNING, durationInMinutes = 15),
+          Event(title = "Learn about Kotlin", daypart = DayPart.AFTERNOON, durationInMinutes = 30),
+          Event(title = "Practice Compose", daypart = DayPart.AFTERNOON, durationInMinutes = 60),
+          Event(
+              title = "Watch latest DevBytes video",
+              daypart = DayPart.AFTERNOON,
+              durationInMinutes = 10
+          ),
+          Event(
+              title = "Check out latest Android Jetpack library",
+              daypart = DayPart.EVENING,
+              durationInMinutes = 45
+          ),
       )
-  )
 }
