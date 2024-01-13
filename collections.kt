f4bf2c44d439dcf994acdf2c732a603289f3897f
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-13 15:25:45 -0500

    demonstrate array random access and assignment

--- collections.kt
+++ collections.kt
@@ -3,4 +3,15 @@ fun main() {
   val rockPlanets = arrayOf("Mercury", "Venus", "Earth", "Mars")
 
   val solarSystem = rockPlanets + gasPlanets
+
+  solarSystem[3] = "Little Earth"
+
+  println(solarSystem[0])
+  println(solarSystem[1])
+  println(solarSystem[2])
+  println(solarSystem[3])
+  println(solarSystem[4])
+  println(solarSystem[5])
+  println(solarSystem[6])
+  println(solarSystem[7])
 }
